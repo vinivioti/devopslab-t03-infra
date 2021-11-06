c1db0e2b31a1f0e9fef26a43ccdc27ce6b42f1ec
Gaby Dias <gabydias@gmail.com>
2021-11-06 09:29:28 -0300

    Primeiro Commit

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.89.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  project = "labdevopscloudt03"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
